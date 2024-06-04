79c238b38d70ad8dd48ececabccd3494f270de03
Nitishrai1 <nitish.12208500@lpu.in>
2024-06-04 11:09:47 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// setup to express app
+const express=require("express");
+const path=require("path");
+const app=express();
+
+// setting the view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+// serving the static file from the public directory
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// Defin routes
+const indexroute=require('./routes/index');
+const aboutroute=require('./routes/about');
+
+
+app.use('/',indexroute);
+app.use('/',aboutroute);
+
+
+// creating the server
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
